723c95ca0569438b14ff3025fad570c503880ac2
ntinos2129 <kneymarmag@gmail.com>
2023-10-19 23:02:54 +0300

    photosensitivity game
    epilepsy test

--- game.c
+++ game.c
@@ -8,65 +8,31 @@
 int main()
 {
     int screenWidth = 800;
-    int screenHeight = 450;
+    int screenHeight = 600;
 
-    SetConfigFlags(FLAG_MSAA_4X_HINT);
+
     InitWindow(screenWidth, screenHeight, "raygame");
-    InitPhysics();
+    
 
-    // Create floor and walls rectangle physics body
-    PhysicsBody floor = CreatePhysicsBodyRectangle((Vector2){ screenWidth/2, screenHeight }, screenWidth, 100, 10);
-    PhysicsBody platformLeft = CreatePhysicsBodyRectangle((Vector2){ screenWidth*0.25f, screenHeight*0.6f }, screenWidth*0.25f, 10, 10);
-    PhysicsBody platformRight = CreatePhysicsBodyRectangle((Vector2){ screenWidth*0.75f, screenHeight*0.6f }, screenWidth*0.25f, 10, 10);
-    PhysicsBody wallLeft = CreatePhysicsBodyRectangle((Vector2){ -5, screenHeight/2 }, 10, screenHeight, 10);
-    PhysicsBody wallRight = CreatePhysicsBodyRectangle((Vector2){ screenWidth + 5, screenHeight/2 }, 10, screenHeight, 10);
-    floor->enabled = false;
-    platformLeft->enabled = false;
-    platformRight->enabled = false;
-    wallLeft->enabled = false;
-    wallRight->enabled = false;
-    PhysicsBody body = CreatePhysicsBodyRectangle((Vector2){ screenWidth/2, screenHeight/2 }, 50, 50, 1);
-    body->freezeOrient = true;
+
     
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
-        if (IsKeyDown(KEY_RIGHT))
-            body->velocity.x = VELOCITY;
-        else if (IsKeyDown(KEY_LEFT))
-            body->velocity.x = -VELOCITY;
-
-        if (IsKeyDown(KEY_UP) && body->isGrounded)body->velocity.y = -VELOCITY*4;
+        
         BeginDrawing();
-
-        ClearBackground(BLACK);
-
-        DrawFPS(screenWidth - 90, screenHeight - 30);
-
-        // Draw created physics bodies
-        int bodiesCount = GetPhysicsBodiesCount();
-        for (int i = 0; i < bodiesCount; i++)
-        {
-            PhysicsBody body = GetPhysicsBody(i);
-
-            int vertexCount = GetPhysicsShapeVerticesCount(i);
-            for (int j = 0; j < vertexCount; j++)
-            {
-                // Get physics bodies shape vertices to draw lines
-                // Note: GetPhysicsShapeVertex() already calculates rotation transformations
-                Vector2 vertexA = GetPhysicsShapeVertex(body, j);
-
-                int jj = (((j + 1) < vertexCount) ? (j + 1) : 0);   // Get next vertex or first to close the shape
-                Vector2 vertexB = GetPhysicsShapeVertex(body, jj);
-
-                DrawLineV(vertexA, vertexB, GREEN);     // Draw a line between two vertex positions
-            }
+        if (IsKeyDown(KEY_SPACE)){
+            ClearBackground(WHITE);
         }
-        DrawText("Use 'ARROWS' to move player", 10, 10, 10, WHITE);
+        else{
+            ClearBackground(BLACK);
+        }
+        
+
+        //DrawText("Use 'ARROWS' to move player", 10, 10, 10, WHITE);
         EndDrawing();
     }
 
-    ClosePhysics();
     CloseWindow();
 
     return 0;
